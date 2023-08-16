2e6d0ac2ea13c5ba2bd86e10dcfb57e77daa2c38
Pylgos <43234674+Pylgos@users.noreply.github.com>
2023-08-16 11:34:55 +0900

    change sync target

--- sync.bash
+++ sync.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-rsync -a . robotclub-e10:/home/robotclub/work/ros \
+rsync -a . robotclub-latitude:/home/robotclub/work/ros \
   --info=progress2 \
   --exclude={.vscode,log,build} \
   --delete \
