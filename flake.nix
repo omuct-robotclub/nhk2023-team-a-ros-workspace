7d960da78413d5e1d0eff9cdb199b635b481ca56
Pylgos <43234674+Pylgos@users.noreply.github.com>
2024-12-10 13:00:25 +0900

    devshell: Add nim compiler

--- flake.nix
+++ flake.nix
@@ -8,14 +8,90 @@
     colcon-ros-nimble.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, flake-utils, nixpkgs, ros2nix, colcon-ros-nimble }: 
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+      ros2nix,
+      colcon-ros-nimble,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        nim-overlay = final: prev: {
+          nim = final.callPackage ./nim { inherit nixpkgs; };
+        };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ nim-overlay ];
+        };
         rosPkgs = ros2nix.legacyPackages.${system}.humble;
         py = pkgs.python310Packages;
         colconRosNimble = colcon-ros-nimble.packages.${system}.default;
-      in rec {
+        cligen = pkgs.fetchFromGitHub {
+          owner = "c-blake";
+          repo = "cligen";
+          rev = "1.7.8";
+          hash = "sha256-iImmHie0hMpKGdA8uRMLOXbbUlID68w/8ABhi4OyqKU=";
+        };
+        nimble-ament-build = pkgs.stdenv.mkDerivation {
+          pname = "nimble-ament-build";
+          version = "0.1.0";
+          src = pkgs.fetchFromGitHub {
+            owner = "Pylgos";
+            repo = "nimble_ament_build";
+            rev = "d05319487b234d87a071b4c7d6af7fe9f59291af";
+            hash = "sha256-Nvx0Imbv0duApAMZ9WuDQcjkBNShP+pu3d29id0LNGo=";
+          };
+          nativeBuildInputs = [ pkgs.nim ];
+          buildPhase = ''
+            mkdir -p $out/bin
+            nim c -d:release -o:$out/bin/nimble-ament-build -p:${cligen} --nimcache:nimcache src/nimble_ament_build.nim
+          '';
+          installPhase = "true";
+        };
+        nimble-unwrapped = pkgs.stdenv.mkDerivation rec {
+          pname = "nimble-unwrapped";
+          version = "0.13.1";
+          strictDeps = true;
+
+          src = pkgs.fetchFromGitHub {
+            owner = "nim-lang";
+            repo = "nimble";
+            rev = "v${version}";
+            sha256 = "1idb4r0kjbqv16r6bgmxlr13w2vgq5332hmnc8pjbxiyfwm075x8";
+          };
+
+          depsBuildBuild = [ pkgs.nim ];
+          buildInputs = [ pkgs.openssl ];
+
+          nimFlags = [
+            "-d:release"
+          ];
+
+          buildPhase = ''
+            runHook preBuild
+            HOME=$NIX_BUILD_TOP nim c $nimFlags src/nimble
+            runHook postBuild
+          '';
+
+          installPhase = ''
+            runHook preBuild
+            install -Dt $out/bin src/nimble
+            runHook postBuild
+          '';
+
+          meta = with pkgs.lib; {
+            description = "Package manager for the Nim programming language";
+            homepage = "https://github.com/nim-lang/nimble";
+            license = licenses.bsd3;
+            maintainers = with maintainers; [ ehmry ];
+            mainProgram = "nimble";
+          };
+        };
+      in
+      rec {
         devShells.default = rosPkgs.mkRosWorkspace {
           pkgs = [
             colconRosNimble
@@ -60,10 +136,13 @@
             pkgs.gdb
             pkgs.cairo
             pkgs.clang-tools
+            pkgs.nim
+            nimble-unwrapped
+            nimble-ament-build
           ];
 
           buildInputs = [
-            
+
           ];
 
           shellHook = ''
